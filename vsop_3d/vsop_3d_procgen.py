7ffdf368a99183a2828d0802c32fbe3020b0ddd0
Max Weltevrede <maxweltevrede@gmail.com>
2024-12-20 17:29:33 +0100

    Pre-sample starting states and teleport to them when resetting env

--- vsop_3d/vsop_3d_procgen.py
+++ vsop_3d/vsop_3d_procgen.py
@@ -222,6 +222,12 @@ def parse_args():
         help="maximum number of pure exploration steps to take each episode",
     )
     parser.add_argument(
+        "--num_starting_states",
+        type=int,
+        default=1,
+        help="number of starting states to pre-sample with pure-exploration"
+    )
+    parser.add_argument(
         "--num_checkpoints",
         type=int,
         default=20,
@@ -403,7 +409,45 @@ class Agent(nn.Module):
             action = probs.sample()
         return action, probs.log_prob(action), probs.entropy(), self.critic(x)
 
+class VecSettableProcgen(gym.Wrapper):
+    def __init__(self, env: gym.Env):
+        """
+        Environment wrapper that defines a function that allows for getting and setting states in a Procgen VecEnv
+        """
+        super(VecSettableProcgen, self).__init__(env)
 
+    def get_states(self):
+        unwrapped = self.env
+        while isinstance(unwrapped, gym.Wrapper):
+            unwrapped = unwrapped.env
+        return unwrapped.env.get_state()
+        
+    def set_states(self, states, env_indices):
+        new_states = self.get_states()
+        for c, v in enumerate(env_indices):
+            new_states[v] = states[c]
+        
+        unwrapped = self.env
+        while isinstance(unwrapped, gym.Wrapper):
+            unwrapped = unwrapped.env
+        unwrapped.env.set_state(new_states)
+
+        if isinstance(self.env, gym.wrappers.FrameStack):
+            # reset framestack for env_indices whose states got set
+            obs = unwrapped.env.observe()[1]['rgb'][env_indices]
+            for _ in range(self.env.num_stack):
+                current_obs = self.env.frames[0]
+                current_obs[env_indices] = obs
+                self.env.frames.append(current_obs)
+
+        return self.env.observation()
+        
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
+    
+    def step(self, action):
+        return self.env.step(action)
+    
 def make_envs(num_envs, env_id, num_levels, gamma, distribution_mode="easy", frames=4, start_level=0):
     envs = ProcgenEnv(
         num_envs=num_envs,
@@ -423,33 +467,13 @@ def make_envs(num_envs, env_id, num_levels, gamma, distribution_mode="easy", fra
     envs = gym.wrappers.NormalizeReward(envs, gamma=gamma)
     envs = gym.wrappers.TransformReward(envs, lambda reward: np.clip(reward, -10, 10))
     envs = gym.wrappers.FrameStack(envs, frames)
+    envs = VecSettableProcgen(envs)
     return envs
-
-def has_full_episode(queue):
-    first_done = -1
-    second_done = -1
-    for i, experience_tuple in enumerate(queue):
-        if experience_tuple[1]:
-            if first_done == -1:
-                first_done = i
-            elif second_done == -1:
-                second_done = i
-                break
-    return first_done >=0 and second_done >=0, first_done, second_done
-
-def reset_frame_stack(env, idx):
-    obs = env.frames[-1][idx]
-    for _ in range(env.num_stack):
-        current_obs = env.frames[0]
-        current_obs[idx] = obs
-        env.frames.append(current_obs)
-    return torch.Tensor(np.array(env.observation())).byte().to("cpu")[:, idx]
 
 def main():
     args = parse_args()
     exp_name = f"vsop-3d-{args.experiment_id}"
     run_experiment(exp_name=exp_name, args=args)
-
 
 def run_experiment(exp_name, args):
     job_dir = Path(args.job_dir)
@@ -545,6 +569,58 @@ def run_experiment(exp_name, args):
     # pure_actor_critic_weights = torch.load(f"{args.env_id}-expgen.pt")
     pure_actor_critic.load_state_dict(pure_actor_critic_weights['state_dict'])
 
+    # Pre-sample reachable starting states
+    starting_states = [list() for _ in range(args.num_training_levels)] 
+    sequence_lengths = np.linspace(0, args.max_pure_expl_steps, args.num_starting_states).astype(int)
+    for i in range(args.num_training_levels):
+        for j in range(args.num_starting_states-1):
+            temp_envs = make_envs(
+                num_envs=1,
+                env_id=args.env_id,
+                num_levels=1,
+                start_level=i,
+                gamma=0.999,
+                frames=16,
+            )
+            next_obs = torch.Tensor(np.array(temp_envs.reset())).to(device)
+            pure_masks = torch.ones(1, 1, device=device)
+            pure_recurrent_hidden_states = torch.zeros(1, pure_actor_critic.recurrent_hidden_state_size, device=device)
+
+            # pure exploration
+            if len(starting_states[i]) == 0:
+                starting_states[i].append((temp_envs.get_states(), 0))
+
+            step = 0
+            while step < sequence_lengths[j+1]:
+                with torch.no_grad():
+                    _, pure_action, _, _, pure_recurrent_hidden_states = pure_actor_critic.act(
+                        next_obs[-1].permute(0,3,1,2) / 255.,
+                        pure_recurrent_hidden_states,
+                        pure_masks
+                        )
+
+                # TRY NOT TO MODIFY: execute the game and log data.
+                next_obs, _, done, _ = temp_envs.step(pure_action.squeeze(-1).cpu().numpy())
+                step += 1
+
+                next_obs, next_done = (
+                    torch.Tensor(np.array(next_obs)).to(device),
+                    torch.Tensor(done).to(device),
+                )
+                pure_masks = torch.FloatTensor([[0.0] if done_ else [1.0] for done_ in done]).to(device)
+
+                if done[0]:
+                    # print("Done when exploring")
+                    step = 0
+            
+            starting_states[i].append((temp_envs.get_states(), sequence_lengths[j+1]))
+
+    del pure_actor_critic
+    del pure_actor_critic_weights
+    del pure_masks
+    del pure_recurrent_hidden_states
+    torch.cuda.empty_cache()
+
     # ALGO Logic: Storage setup
     obs = (
         torch.zeros(
@@ -561,27 +637,27 @@ def run_experiment(exp_name, args):
     rewards = torch.zeros((args.num_steps, args.num_envs)).to(device)
     dones = torch.zeros((args.num_steps, args.num_envs)).to(device)
 
-    ##
-    ## Pure exploration
-    ##
-    # a temporary buffer that queues all the collected experiences outside of the pure exploration phase
-    # before it is added to the rollout buffer
-    tmp_rollout_buffer = [deque() for _ in range(args.num_envs)]
-    max_pure_expl_steps = args.max_pure_expl_steps
-    num_pure_expl_steps = np.random.randint(0, max_pure_expl_steps+1 , size=args.num_envs)
-    episode_steps = np.zeros(args.num_envs)
-    num_normal_steps = 0
-    num_dones_in_queue = np.zeros(args.num_envs)
-    pure_masks = torch.ones(args.num_envs, 1, device=device)
-    pure_recurrent_hidden_states = torch.zeros(args.num_envs, pure_actor_critic.recurrent_hidden_state_size, device=device)
-
     # TRY NOT TO MODIFY: start the game
     global_step = 0
+    non_teleport_step = 0
     start_time = time.time()
     next_obs = torch.Tensor(np.array(envs.reset())).to(device)
     next_obs_test = torch.Tensor(np.array(envs_test.reset())).to(device)
     next_done = torch.zeros(args.num_envs).to(device)
     num_updates = args.total_timesteps // args.batch_size
+
+    def sample_starting_states(starting_states, num_to_sample):
+        states = []
+        total_steps = 0
+        for _ in range(num_to_sample):
+            s, l = starting_states[np.random.randint(args.num_training_levels)][np.random.randint(args.num_starting_states)]
+            states.append(s)
+            total_steps += l
+        return states, total_steps
+    
+    new_states, total_steps = sample_starting_states(starting_states, args.num_envs)
+    non_teleport_step += total_steps
+    next_obs = torch.Tensor(np.array(envs.set_states(new_states, list(range(args.num_envs))))).to(device)
 
     test_returns = []
 
@@ -591,23 +667,21 @@ def run_experiment(exp_name, args):
         agent.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         global_step = checkpoint['global_step']
+        non_teleport_step = checkpoint['non_teleport_step']
         num_updates = (args.total_timesteps - global_step) // args.batch_size
 
     for update in range(1, num_updates + 1):
         if not args.thompson:
             agent.eval()
-        n_steps_added_to_buffer = 0
         episode_returns = []
         episode_len = []
         test_episode_returns = []
         test_episode_len = []
-        while n_steps_added_to_buffer < args.num_steps:
-            experience = []
+        for step in range(0, args.num_steps):
             global_step += 1 * args.num_envs
-            # obs[step] = next_obs.byte().to("cpu")
-            # dones[step] = next_done
-            experience.append(next_obs.byte().to("cpu"))
-            experience.append(next_done)
+            non_teleport_step += 1 * args.num_envs
+            obs[step] = next_obs.byte().to("cpu")
+            dones[step] = next_done
 
             # ALGO LOGIC: action logic
             with torch.no_grad():
@@ -615,36 +689,18 @@ def run_experiment(exp_name, args):
                 action, logprob, _ = agent.get_action(next_obs)
                 agent.eval()
                 action_test, _, _ = agent.get_action(next_obs_test)
-            # actions[step] = action
-            # logprobs[step] = logprob
-            experience.append(action)
-            experience.append(logprob)
-
-            normal_inds = episode_steps >= num_pure_expl_steps
-            pure_inds = episode_steps < num_pure_expl_steps
-            # random pure exploration for now
-            if sum(pure_inds) > 0:
-                with torch.no_grad():
-                    _, pure_action, _, _, pure_recurrent_hidden_states[pure_inds] = pure_actor_critic.act(
-                        next_obs[-1,pure_inds].permute(0,3,1,2),
-                        pure_recurrent_hidden_states[pure_inds],
-                        pure_masks[pure_inds]
-                        )
-                action[pure_inds] = pure_action.squeeze(-1)
+            actions[step] = action
+            logprobs[step] = logprob
 
             # TRY NOT TO MODIFY: execute the game and log data.
             next_obs, reward, done, info = envs.step(action.cpu().numpy())
             next_obs_test, _, _, info_test = envs_test.step(action_test.cpu().numpy())
-            # rewards[step] = torch.tensor(reward).to(device).view(-1)
-            experience.append(reward)
+            rewards[step] = torch.tensor(reward).to(device).view(-1)
             next_obs, next_done, next_obs_test = (
                 torch.Tensor(np.array(next_obs)).to(device),
                 torch.Tensor(done).to(device),
                 torch.Tensor(np.array(next_obs_test)).to(device),
             )
-            pure_masks = torch.FloatTensor([[0.0] if done_ else [1.0] for done_ in done]).to(device)
-            # Set mask to 0 for the last pure exploration step of the episode
-            pure_masks[episode_steps == num_pure_expl_steps-1] = torch.FloatTensor([0.0]).to(device)
 
             for item in info:
                 if "episode" in item.keys():
@@ -658,94 +714,11 @@ def run_experiment(exp_name, args):
                     test_episode_returns.append(item["episode"]["r"])
                     test_episode_len.append(item["episode"]["l"])
                     break
-
-            # add non-pure exploration experience to the tmp buffer
-            for idx, normal in enumerate(normal_inds):
-                if normal:
-                    if episode_steps[idx] == num_pure_expl_steps[idx]:
-                        episode_just_started = torch.tensor(1.).to(device)
-                        # reset the frame-stack history
-                        obs_to_add = reset_frame_stack(envs, idx)
-                    else:
-                        episode_just_started = torch.tensor(0.).to(device)
-                        obs_to_add = experience[0][:,idx]
-                    experience_tuple = (obs_to_add, episode_just_started, experience[2][idx], experience[3][idx], experience[4][idx])
-                    tmp_rollout_buffer[idx].append(experience_tuple)
-                    num_dones_in_queue[idx] += 1 if episode_just_started else 0
-
-            # add experience to rollout buffer if possible
-            if np.all(np.array([not len(q) == 0 for q in tmp_rollout_buffer])):
-                obs_list = []
-                dones_list = []
-                action_list = []
-                log_probs_list = []
-                reward_list = []
-                for idx in range(args.num_envs):
-                    experience_tuple = tmp_rollout_buffer[idx].popleft()
-                    obs_list.append(experience_tuple[0])
-                    dones_list.append(experience_tuple[1])
-                    action_list.append(experience_tuple[2])
-                    log_probs_list.append(experience_tuple[3])
-                    reward_list.append(experience_tuple[4])
-                    num_dones_in_queue[idx] += -1 if experience_tuple[1] else 0
-                obs_list = torch.stack(obs_list, dim=1)
-                action_list = torch.stack(action_list, dim=0)
-                reward_list = torch.tensor(np.stack(reward_list, axis=0)).to(device).view(-1)
-                dones_list = torch.stack(dones_list, dim=0)
-                log_probs_list = torch.stack(log_probs_list, dim=0)
-
-                obs[n_steps_added_to_buffer] = obs_list
-                dones[n_steps_added_to_buffer] = dones_list
-                actions[n_steps_added_to_buffer] = action_list
-                logprobs[n_steps_added_to_buffer] = log_probs_list
-                rewards[n_steps_added_to_buffer] = reward_list
-                n_steps_added_to_buffer += 1
-                num_normal_steps += 1 * args.num_envs
-            else:
-                # take full episode from other queue (preferably the largest) and add it to the smallest queue
-                sorted_queue_inds = sorted(range(args.num_envs), key=lambda x: len(tmp_rollout_buffer[x]), reverse=True)
-                found_full_episode = False
-                for i in sorted_queue_inds:
-                    if num_dones_in_queue[i] < 2:
-                        continue
-                    queue = tmp_rollout_buffer[i]
-                    has_episode, first_done, second_done = has_full_episode(queue)
-                    if has_episode:
-                        found_full_episode = True
-                        break
-                
-                if found_full_episode:
-                    num_dones_in_queue[[len(q) for q in tmp_rollout_buffer].index(0)] += 1
-                    num_dones_in_queue[i] += -1
-
-                    empty_queue = tmp_rollout_buffer[[len(q) for q in tmp_rollout_buffer].index(0)]
-                    queue.rotate(-1*first_done)
-                    for _ in range(second_done - first_done):
-                        empty_queue.append(queue.popleft())
-                    queue.rotate(first_done)
                     
-
-            episode_steps += 1
-            for idx, d in enumerate(done):
-                if d:
-                    episode_steps[idx] = 0
-                    num_pure_expl_steps[idx] = np.random.randint(0, max_pure_expl_steps+1)
-
-        normal_next_obs = next_obs.detach().clone()
-        normal_next_done = next_done.detach().clone()
-        for idx in range(len(tmp_rollout_buffer)):
-            if not len(tmp_rollout_buffer[idx]) == 0:
-                normal_next_obs[:, idx] = tmp_rollout_buffer[idx][0][0]
-                normal_next_done[idx] = tmp_rollout_buffer[idx][0][1]
-
-        # empty the queues since that data will no longer be on-policy for the next iteration
-        steps_thrown_out = 0
-        for q in tmp_rollout_buffer:
-            steps_thrown_out += len(q)
-            q.clear()
-        num_dones_in_queue = np.zeros(args.num_envs)
-        # don't count the steps we threw out (we only had to throw them out due to implementation issues with Procgen)
-        global_step -= steps_thrown_out
+            if np.any(done):
+                new_states, total_steps = sample_starting_states(starting_states, done.sum())
+                non_teleport_step += total_steps
+                next_obs = torch.Tensor(np.array(envs.set_states(new_states, np.where(done)[0]))).to(device)
 
         # bootstrap value if not done
         b_obs = obs.permute(1, 0, 2, 3, 4, 5).reshape(
@@ -758,7 +731,7 @@ def run_experiment(exp_name, args):
         b_advantages, b_returns = [], []
         dl = torch.utils.data.DataLoader(
             torch.utils.data.TensorDataset(
-                torch.cat([b_obs, normal_next_obs.byte().to("cpu")], dim=1).permute(
+                torch.cat([b_obs, next_obs.byte().to("cpu")], dim=1).permute(
                     1, 0, 2, 3, 4
                 )
             ),
@@ -783,7 +756,7 @@ def run_experiment(exp_name, args):
                 lastgaelam = 0
                 for t in reversed(range(args.num_steps)):
                     if t == args.num_steps - 1:
-                        nextnonterminal = 1.0 - normal_next_done
+                        nextnonterminal = 1.0 - next_done
                         nextvalues = next_value
                     else:
                         nextnonterminal = 1.0 - dones[t + 1]
@@ -865,6 +838,7 @@ def run_experiment(exp_name, args):
         if not os.path.isfile(checkpoint_path):
             torch.save({
                 'global_step': global_step,
+                'non_teleport_step': non_teleport_step,
                 'model_state_dict': agent.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
             }, checkpoint_path)
@@ -883,8 +857,7 @@ def run_experiment(exp_name, args):
         writer.add_scalar(
             "charts/SPS", int(global_step / (time.time() - start_time)), global_step
         )
-        writer.add_scalar("charts/num_experiences_in_queue", sum([len(q) for q in tmp_rollout_buffer]), global_step)
-        writer.add_scalar("charts/num_normal_steps", num_normal_steps, global_step)
+        writer.add_scalar("charts/num_non_teleport_steps", non_teleport_step, global_step)
         writer.add_scalar("train/episodic_return", np.array(episode_returns).mean(), global_step)
         writer.add_scalar("train/episodic_length", np.array(episode_len).mean(), global_step)
         writer.add_scalar("test/episodic_return", np.array(test_episode_returns).mean(), global_step)
